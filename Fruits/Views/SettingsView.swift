51b14b26c063d84aac03aeb00d952b6642c6a6e9
giovannamoeller <giovannagmoeller@gmail.com>
2023-04-24 10:33:37 -0300

    setting label view

--- Fruits/Views/SettingsView.swift
+++ Fruits/Views/SettingsView.swift
@@ -14,8 +14,28 @@ struct SettingsView: View {
   var body: some View {
     NavigationView {
       ScrollView(.vertical, showsIndicators: false) {
-        VStack {
-          Text("Hello, World!")
+        VStack(spacing: 20) {
+          
+          GroupBox {
+            Divider().padding(.vertical, 4)
+            
+            HStack(alignment: .center, spacing: 10) {
+              Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80)
+                .cornerRadius(8)
+              
+              Text("Most fruits are naturally low in fat, sodium and calories. None have cholesterol. Fruits are sources of many essential nutrients, including potassium, dietary fiber, vitamins and much more.")
+                .font(.footnote)
+              
+            }
+            
+          } label: {
+            SettingsLabelView(labelText: "Fruits App", labelImage: "info.circle")
+          }
+
+          
         }
         .navigationTitle("Settings")
         .navigationBarTitleDisplayMode(.large)
@@ -37,5 +57,22 @@ struct SettingsView: View {
 struct SettingsView_Previews: PreviewProvider {
   static var previews: some View {
     SettingsView()
+  }
+}
+
+struct SettingsLabelView: View {
+  
+  var labelText: String
+  var labelImage: String
+  
+  var body: some View {
+    HStack {
+      Text(labelText.uppercased())
+      
+      Spacer()
+      
+      Image(systemName: labelImage)
+    }
+    .fontWeight(.bold)
   }
 }
