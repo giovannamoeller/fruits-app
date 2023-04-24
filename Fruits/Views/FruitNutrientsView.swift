9636495a05cf26d8071e731f3c31b3bf320f45e6
giovannamoeller <giovannagmoeller@gmail.com>
2023-04-24 10:05:37 -0300

    applying modifiers to nutrition box

--- Fruits/Views/FruitNutrientsView.swift
+++ Fruits/Views/FruitNutrientsView.swift
@@ -16,12 +16,20 @@ struct FruitNutrientsView: View {
     GroupBox() {
       DisclosureGroup("Nutritional value per 100g") {
         ForEach(0..<nutrients.count, id: \.self) { item in
+          Divider().padding(.vertical, 2)
           HStack {
-            Text(nutrients[item])
+            Group {
+              Image(systemName: "info.circle")
+              
+              Text(nutrients[item])
+            }
+            .foregroundColor(fruit.gradientColors[1])
+            .bold()
             
-            Spacer()
+            Spacer(minLength: 25)
             
             Text(fruit.nutrition[item])
+              .multilineTextAlignment(.trailing)
           }
         }
       }
