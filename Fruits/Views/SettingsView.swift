67ce6ba4721c433fc028362f7bb5c7dc72b58da2
giovannamoeller <giovannagmoeller@gmail.com>
2023-04-24 10:43:03 -0300

    customization box - restart application

--- Fruits/Views/SettingsView.swift
+++ Fruits/Views/SettingsView.swift
@@ -10,12 +10,13 @@ import SwiftUI
 struct SettingsView: View {
   
   @Environment(\.presentationMode) var presentationMode
+  @AppStorage("isOnboarding") var isOnboarding: Bool = false
+  @State private var showOnboarding: Bool = false
   
   var body: some View {
     NavigationView {
       ScrollView(.vertical, showsIndicators: false) {
         VStack(spacing: 20) {
-          
           GroupBox {
             Divider().padding(.vertical, 4)
             
@@ -28,14 +29,28 @@ struct SettingsView: View {
               
               Text("Most fruits are naturally low in fat, sodium and calories. None have cholesterol. Fruits are sources of many essential nutrients, including potassium, dietary fiber, vitamins and much more.")
                 .font(.footnote)
-              
             }
-            
           } label: {
             SettingsLabelView(labelText: "Fruits App", labelImage: "info.circle")
           }
-
           
+          GroupBox {
+            Divider().padding(.vertical, 4)
+            
+            VStack(spacing: 20) {
+              
+              Text("If you wish, you can restart the application by toggle the switch in this box. That way it starts the onboarding process and you will see the welcome screen again.")
+                .font(.footnote)
+              
+              Toggle("Restarted".uppercased(), isOn: $isOnboarding)
+                .bold()
+                .foregroundColor(.green)
+             
+            }
+          } label: {
+            SettingsLabelView(labelText: "Customization", labelImage: "paintbrush")
+          }
+
         }
         .navigationTitle("Settings")
         .navigationBarTitleDisplayMode(.large)
